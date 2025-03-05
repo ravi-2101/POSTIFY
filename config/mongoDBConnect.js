cc24a794fe9b3ac99f8353821dc5387327a95fe8
ravi-2101 <ravirajmobh.vision@gmail.com>
2025-03-05 13:58:44 +0530

    updated in mongoDB connect

--- config/mongoDBConnect.js
+++ config/mongoDBConnect.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const mongoDBConnect = async () => {
     try {
        await  mongoose.connect(process.env.DATABASE_URL)
-       .then(() => console.log("database connected successfully!"))
+       .then((res) => console.log("database connected successfully!"))
         .catch((err) => console.log(err))
     } catch (error) {
         console.log("database connection error : ",error);
